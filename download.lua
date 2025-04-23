7afb353eb46fe950cd400b4b5a93b5873e10ad6b
defaulito <98851923+defaulito@users.noreply.github.com>
2025-04-24 01:10:28 +0200

    Add files via upload

--- download.lua
+++ download.lua
@@ -1,3 +1,3 @@
-shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/refs/heads/main/powersystem.lua")
-shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/refs/heads/main/startup.lua")
+shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/powersystem.lua")
+shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/startup.lua")
 print("Downloaded files")
