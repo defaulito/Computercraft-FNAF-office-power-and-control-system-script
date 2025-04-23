67b619cac6ef8e349122e4eaf54ac56d6fcde9f0
defaulito <98851923+defaulito@users.noreply.github.com>
2025-04-24 01:04:01 +0200

    added download script

---
+++ download.lua
@@ -0,0 +1,3 @@
+os.run({}, "/rom/programs/http/wget.lua", "https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/refs/heads/main/powersystem.lua")
+os.run({}, "/rom/programs/http/wget.lua", "https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/refs/heads/main/startup.lua")
+print("Downloaded files")
