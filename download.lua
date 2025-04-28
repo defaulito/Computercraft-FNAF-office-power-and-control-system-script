084a8b97c9331f1d893840e760754d38eca98aca
defaulito <98851923+defaulito@users.noreply.github.com>
2025-04-28 13:59:32 +0300

    Add files via upload

--- download.lua
+++ download.lua
@@ -1,17 +1,17 @@
 if fs.exists("powersystem.lua") then
     shell.run("delete powersystem.lua")
     shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/powersystem.lua")
-    print("\nUpdated powersystem.lua")
+    print("Updated powersystem.lua\n")
 else
     shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/powersystem.lua")
-    print("\nDownloaded powersystem.lua")
+    print("Downloaded powersystem.lua\n")
 end
 if fs.exists("startup.lua") then
     shell.run("delete startup.lua")
     shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/startup.lua")
-    print("\nUpdated startup.lua")
+    print("Updated startup.lua\n")
 else
     shell.run("wget https://raw.githubusercontent.com/defaulito/Computercraft-FNAF-office-power-and-control-system-script/main/startup.lua")
-    print("\nDownloaded startup.lua")
+    print("Downloaded startup.lua\n")
 end
 print("Done. restart computer to run script, make sure the modems on the monitor and computer are activated")
